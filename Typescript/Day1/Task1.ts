cc51f6ce132c3ec5c02a97e6150d5c385071b496
PrachiK5 <104131870+PrachiK5@users.noreply.github.com>
2022-04-26 14:51:42 +0530

    Add files via upload

---
+++ Typescript/Day1/Task1.ts
@@ -0,0 +1,18 @@
+let EmployeeId : number = 5464654;
+
+let EmployeeName : string = 'Scott';
+
+let EmployeeJob : string = 'Manager';
+
+let isPermanentEmployee : boolean = true;
+
+let DeptNo: number = 10; 
+
+console.log("Employee Id :" + EmployeeId);
+console.log("Employee Name :" + EmployeeName);
+console.log("Employee Job :" + EmployeeJob);
+console.log("Employee Id :" + isPermanentEmployee);
+console.log("Employee Deptno :" + DeptNo);
+
+console.log("Employee Id :" + EmployeeId + " " + "Employee Name :" + EmployeeName + " " + "Employee Job :" + EmployeeJob+ " " + "Employee Id :" + isPermanentEmployee + " " + "Employee Deptno :" + DeptNo);
+
---
+++ Typescript/Day1/Task2.ts
@@ -0,0 +1,36 @@
+//Array.map()
+/*
+map() creates a new array from calling a function for every array element.
+
+map() calls a function once for each element in an array.
+
+map() does not execute the function for empty elements.
+
+map() does not change the original array.
+*/
+
+//Array.find()
+/*
+The find() method returns the value of the first element that passes a test.
+
+The find() method executes a function for each array element.
+
+The find() method returns undefined if no elements are found.
+
+The find() method does not execute the function for empty elements.
+
+The find() method does not change the original array.
+*/
+
+//Array.findIndex()
+/*
+The findIndex() method executes a function for each array element.
+
+The findIndex() method returns the index (position) of the first element that passes a test.
+
+The findIndex() method returns -1 if no match is found.
+
+The findIndex() method does not execute the function for empty array elements.
+
+The findIndex() method does not change the original array.
+*/
